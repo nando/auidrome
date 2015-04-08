a88ad30ceac1e09ca319e11a1c0e4863f45a9384
Fernando Garcia Samblas <fernando.gs@gmail.com>
2015-04-08 05:31:06 +0000

    tuit's created_at from index file always as string

--- lib/auidrome/tuit.rb
+++ lib/auidrome/tuit.rb
@@ -29,7 +29,7 @@ module Auidrome
     end
 
     def self.create! tuit, timestamp
-      @@stored_tuits[tuit] = timestamp
+      @@stored_tuits[tuit] = timestamp.to_s # the same as if it're read from JSON file
       Tuit.store_tuits!
     end
 
