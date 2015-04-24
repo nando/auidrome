edf1bf0b10ea2f325ffa0797f0a9c8f6b6019d6f
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2015-04-24 17:54:42 +0200

    fix to upload an image to a 'better' subdirectory

--- bin/uploadphoto.sh
+++ bin/uploadphoto.sh
@@ -4,8 +4,8 @@ if [ $# -eq 2 ]; then
   drome=$1
   image=$2
 
-  echo "scp $image otaony.com:dromes/$drome/public/images/"
-  scp       $image otaony.com:dromes/$drome/public/images/
+  echo "scp $image otaony.com:dromes/$drome/public/images/$image"
+  scp       $image otaony.com:dromes/$drome/public/images/$image
 else
   echo 'Usage: uploadphoto.sh DROME FILE'
 fi
